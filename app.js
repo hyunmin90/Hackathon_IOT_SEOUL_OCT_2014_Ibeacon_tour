a10dd5ec73b0ccff72b1890ddf20deea1a304c96
Steve <steve.wang@daumkakao.com>
2014-11-13 23:11:12 +0900

    add oauth2.0 login callback fix passport app.use included

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.session({ secret: 'your secret here' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
