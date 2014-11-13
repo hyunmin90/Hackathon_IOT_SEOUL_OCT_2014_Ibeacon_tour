6639e6a41de6e5f6858f14690ff1292f2e1454de
Steve <steve.wang@daumkakao.com>
2014-11-13 23:40:17 +0900

    login seperated router

--- app.js
+++ app.js
@@ -7,42 +7,13 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login')
 
-
-var passport = require('passport')
-    , FacebookStrategy = require('passport-facebook').Strategy;
-
-// serialize
-// 인증후 사용자 정보를 세션에 저장
-passport.serializeUser(function(user, done) {
-    console.log('serialize');
-    console.log(user);
-    done(null, user);
-});
-
-
-// deserialize
-// 인증후, 사용자 정보를 세션에서 읽어서 request.user에 저장
-passport.deserializeUser(function(user, done) {
-    //findById(id, function (err, user) {
-    console.log('deserialize');
-    done(null, user);
-    //});
-});
 
 var app = express();
 
 
-passport.use(new FacebookStrategy({
-        clientID: ' 410689979097098 ',
-        clientSecret: '47d689b5c9a71d166e87fa2d9dd30ab7',
-        callbackURL: "http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/auth/facebook/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        done(null,profile);
-    }
-));
+
 
 
 
@@ -62,24 +33,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { successRedirect: '/login_success',
-        failureRedirect: '/login_fail' }));
-app.get('/login_success', ensureAuthenticated, function(req, res){
-
-    res.send(req.user);
-});
-app.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/');
-});
-function ensureAuthenticated(req, res, next) {
-    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
-    if (req.isAuthenticated()) { return next(); }
-    // 로그인이 안되어 있으면, login 페이지로 진행
-    res.redirect('/');
-}
+app.use('/login',login);
 
 
 
