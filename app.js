7f408bc05cd1ab0b99f5091aa5cea8d97080fd0e
Steve <steve.wang@daumkakao.com>
2014-11-14 00:05:17 +0900

    login seperated router

--- app.js
+++ app.js
@@ -9,43 +9,14 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 
-var passport = require('passport')
-    , FacebookStrategy = require('passport-facebook').Strategy;
 
-// serialize
-// 인증후 사용자 정보를 세션에 저장
-passport.serializeUser(function(user, done) {
-    console.log('serialize');
-    console.log(user);
-    done(null, user);
-});
-
-
-// deserialize
-// 인증후, 사용자 정보를 세션에서 읽어서 request.user에 저장
-passport.deserializeUser(function(user, done) {
-    //findById(id, function (err, user) {
-    console.log('deserialize');
-    done(null, user);
-    //});
-});
 
 var app = express();
 
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy;
 
-passport.use(new FacebookStrategy({
-        clientID: ' 410689979097098 ',
-        clientSecret: '47d689b5c9a71d166e87fa2d9dd30ab7',
-        callbackURL: "http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/auth/facebook/callback"
-    },
-    function(accessToken, refreshToken, profile, done) {
-        console.log(profile);
-        done(null,profile);
-    }
-));
 
-passport.authenticate('facebook', { successRedirect: '/login/login_success',
-failureRedirect: '/login/login_fail' });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
