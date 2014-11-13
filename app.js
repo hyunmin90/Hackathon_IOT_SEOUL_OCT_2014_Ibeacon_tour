2250564b4c037092e67ebe9718f1a288608b0fae
Steve <steve.wang@daumkakao.com>
2014-11-13 22:54:32 +0900

    add oauth2.0 login

--- app.js
+++ app.js
@@ -8,7 +8,42 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+var passport = require('passport')
+    , FacebookStrategy = require('passport-facebook').Strategy;
+
+// serialize
+// 인증후 사용자 정보를 세션에 저장
+passport.serializeUser(function(user, done) {
+    console.log('serialize');
+    done(null, user);
+});
+
+
+// deserialize
+// 인증후, 사용자 정보를 세션에서 읽어서 request.user에 저장
+passport.deserializeUser(function(user, done) {
+    //findById(id, function (err, user) {
+    console.log('deserialize');
+    done(null, user);
+    //});
+});
+
 var app = express();
+
+
+passport.use(new FacebookStrategy({
+        clientID: ' 410689979097098 ',
+        clientSecret: '47d689b5c9a71d166e87fa2d9dd30ab7',
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        done(null,profile);
+    }
+));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -45,6 +80,26 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { successRedirect: '/login_success',
+        failureRedirect: '/login_fail' }));
+app.get('/login_success', ensureAuthenticated, function(req, res){
+    res.send(req.user);
+});
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+function ensureAuthenticated(req, res, next) {
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+    if (req.isAuthenticated()) { return next(); }
+    // 로그인이 안되어 있으면, login 페이지로 진행
+    res.redirect('/');
+}
+
+
 
 // production error handler
 // no stacktraces leaked to user
