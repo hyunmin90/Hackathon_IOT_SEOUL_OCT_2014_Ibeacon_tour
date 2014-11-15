37f859e2b36abe232799ac71e1558040cfb53ce1
Steve <steve.wang@daumkakao.com>
2014-11-15 17:09:01 +0900

    hi

--- routes/userdatas.js
+++ routes/userdatas.js
@@ -4,9 +4,10 @@ var router = express.Router();
 router.get('/:UIN/cards', function(req, res) { //유저가 가지고있는 Location정보 
 	var userid = req.params.UIN
 	var uuid = req.param("uuid");
+	console.log(uuid);
 	var sql ='select location from UserLocationCard where UIN=?';
 	console.log(sql);
-	var query = dbcon.query(sql,[uuid],function(err,rows){
+	var query = dbcon.query(sql,uuid,function(err,rows){
 		console.log(rows);
         res.json(rows);
 	});
