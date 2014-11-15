0e9d41edce7004a442f27398c5a8b67fc5f37f88
glory <andew.ju@daumkakao.com>
2014-11-15 22:51:26 +0900

    spotinfo

--- routes/spotInfo.js
+++ routes/spotInfo.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var url = require('url');
+
+var ServiceKey='kovsIkiq0a1AX7kkwPnbGwUYEgKwE%2FSssb5ySRZuweChWD61Tjvr33TMUvxAut6w%2Fqmf6GvMv1IPpZwIhXMIXQ%3D%3D';
+var apikey='';
 
 router.get('/getTourSpotInfo', function(req, res) {
 
-	var uuid = req.param("uuid");
+	var uuid = req.param("uuid").toUpperCase();
 	var sql ='select seq from uuidbridgeseq where uuid=?';
 	var query = dbcon.query(sql,[uuid],function(err,rows){
+
 		console.log(err);
-        res.json(rows);
+		var SEQ = rows[0].seq;
+		console.log(err);
+
+		request({ 	method: 'GET',
+					url: 'http://openapi.jejutour.go.kr:8080/openapi/service/TourMapService/getTourMapView?_type=json&ServiceKey='+ServiceKey+'&SEQ='+SEQ,
+				}, function(err, response) {
+					var value = JSON.parse(response.body);
+					var tmName = value.response.body.items.item.tmName;
+					console.log(tmName);
+
+					var tmDescript = value.response.body.items.item.tmDescript;
+
+					request({ method: 'GET',
+							  url: 'https://apis.daum.net/local/v1/search/keyword?apikey=DAUM_LOCAL_DEMO_APIKEY&image=only&query='+encodeURIComponent(tmName),
+					}, function(err, response) {
+						var DaumValue = JSON.parse(response.body);
+						var imageUrl = DaumValue.channel.item[0].imageUrl;
+						res.json({tmName:tmName, tmDescript:tmDescript, imageUrl:imageUrl});
+					});
+				});
 	});
 });
 
