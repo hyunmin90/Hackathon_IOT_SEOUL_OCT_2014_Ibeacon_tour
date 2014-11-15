bb5e766f33ef8bd34bf80f79675425d1fa6c49da
Steve <steve.wang@daumkakao.com>
2014-11-16 02:11:05 +0900

    translat added

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/:searchlocations', function(req, res) {
 
-	res.render('searchpage', { title: 'searchpage'});
+	var location = req.params.searchLocation;
+	console.log(userid);
+	var sql ='select location from carddata where location=?';
+	console.log(sql);
+	var query = dbcon.query(sql,[location],function(err,rows){
+		console.log(rows);
+        res.json(rows);
+	});
 });
 
 module.exports = router;
