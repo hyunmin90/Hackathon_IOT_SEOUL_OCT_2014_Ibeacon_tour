eeb020df98cdd7e2a153e9b52cbde8e1197e585a
Steve <steve.wang@daumkakao.com>
2014-11-16 03:22:01 +0900

    search engine

--- public/js/common.js
+++ public/js/common.js
@@ -91,7 +91,7 @@ btour.UI=
 		var searchterm= $("#search").val();
 		request = $.ajax
 							({
-        						url: "http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/search/location"+searchterm, 	//Json데이터를 받아올 주소 
+        						url: "http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/search/location/"+searchterm, 	//Json데이터를 받아올 주소 
         						type: "get",
         						dataType:"json",
 	       							success: function(results)
