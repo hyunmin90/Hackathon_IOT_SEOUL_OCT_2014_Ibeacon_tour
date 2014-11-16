8f2ce85a42870f29866a2540c6c6cf53f82babc3
Steve <steve.wang@daumkakao.com>
2014-11-16 15:39:00 +0900

    hi

--- routes/userdatas.js
+++ routes/userdatas.js
@@ -17,15 +17,11 @@ router.get('/:uin/:locationame/addspotcard/', function(req, res) { //유저가 �
 	var uin = req.params.uin;
 
 	console.log(location);
-<<<<<<< HEAD
  
 	var sql ="insert into userlocationcard (location_id,UIN,location) (select t1.id,'"+uin+"','"+location+"' from carddata t1 where t1.location='"+location+"')";
-=======
 	
-	var sql ='SELECT c.id AS id,c.seq AS seq,c.imageUrl AS imageUrl, c.mapUrl AS mapUrl, c.location AS location FROM carddata c JOIN userlocationcard l ON c.id = l.location_id WHERE l.uin =?';
->>>>>>> 26d5ed7f19ac162a897cd5e9dee81a35ac6f7e6a
 	console.log(sql);
-	var query = dbcon.query(sql,[userid],function(err,rows){
+	var query = dbcon.query(sql,function(err,rows){
 		console.log(rows);
         res.json(rows);
 	});
