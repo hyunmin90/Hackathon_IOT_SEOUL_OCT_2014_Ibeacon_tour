0cb2bc212e445c9b0f567704c0f6ceb1859b2cb3
gagaun <thebgu0320@gmail.com>
2014-11-16 23:01:09 +0900

    please press ADD

--- routes/userdatas.js
+++ routes/userdatas.js
@@ -22,8 +22,8 @@ router.get('/:uin/:locationame/addspotcard/', function(req, res) { //유저가 �
 	var query = dbcon.query(sql,function(err,rows){
 		console.log(rows);
         res.json(rows);
-        location.href="http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/login/login_success";
 	});
-});
 
+	$(location).attr('href',"http://ec2-54-64-134-27.ap-northeast-1.compute.amazonaws.com:3000/login/login_success")
+});
 module.exports = router;
