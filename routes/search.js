acb82a7f0d1a18d7398f26052fe3152bb12540d6
Steve <steve.wang@daumkakao.com>
2014-11-16 11:01:11 +0900

    card

--- routes/search.js
+++ routes/search.js
@@ -10,9 +10,9 @@ router.get('/location/:searchlocations', function(req, res) {
 	console.log(req.params.searchlocations);
 	var location = req.params.searchlocations;
 	console.log(location);
-	var sql ='select location,imageURL from carddata where location=?';
+	var sql ='select location,imageURL from carddata where location LIKE "'+location+'"%';
 	console.log(sql);
-	var query = dbcon.query(sql,[location],function(err,rows){
+	var query = dbcon.query(sql,function(err,rows){
 		console.log(rows);
         res.json(rows);
 	});
